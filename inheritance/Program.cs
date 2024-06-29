b37577b47f6d9755c854d53f1fd1846954f75a71
gabriel-12345z <godaniels@.st.ug.edu.gh>
2024-06-29 13:15:30 -0700

    first task

---
+++ inheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal ani = new Animal();
+            Dog bulldog = new Dog();
+            Cat kitten = new Cat();
+
+            ani.MakeSound();
+            bulldog.MakeSound();
+            kitten.MakeSound();
+        }
+    }
+
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic Sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+}
